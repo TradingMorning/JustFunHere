296b3391f94fdd34c280c371d23d62f3e8cc5335
TradingMorning <falconspido.com@gmail.com>
2026-08-27 22:23:01 +0530

    Add files via upload

---
+++ downloader.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AutoShortAi — Universal Video Downloader & Auto-Reverse Bridge Module
+──────────────────────────────────────────────────────────────────────
+"""
+
+from downloader2 import *
