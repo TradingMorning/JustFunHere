1228329543088483853e123cb290c5d4e3640a65
TradingMorning <falconspido.com@gmail.com>
2026-08-28 13:29:22 +0530

    Delete downloader.py

--- downloader.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-AutoShortAi — Universal Video Downloader & Cross-Process Reverse Bridge Module
-──────────────────────────────────────────────────────────────────────────────
-"""
-
-from downloader2 import *
